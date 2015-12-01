039f84b0b3730c58db0a17d4b9ca870d0c04ff16
Zach Holland <zacharias.holland@gmail.com>
2015-12-01 12:14:46 -0800

    Moved Triangles class to its own package and finished writing the comments.

--- src/algorithms/triangles/Triangles.java
+++ src/algorithms/triangles/Triangles.java
@@ -1,4 +1,4 @@
-package algorithms.clustering;
+package algorithms.triangles;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
@@ -29,6 +29,8 @@ public class Triangles {
     /**
      * Counts the number of triangles for each edge as well as the total number of edges
      * in a graph using the specified counting algorithm.
+     *
+     * Use the CountAlgorithm enum to specify the algorithm.
      *
      * @param graph     The graph on which the triangles are counted.
      * @param algorithm The algorithm to use to count the triangles.
@@ -64,44 +66,58 @@ public class Triangles {
         // Since the algorithm works by removing vertices from the graph, we need to operate on
         // a copy of the original graph to prevent the original graph from changing which might
         // be unexpected for the user.
-        Graph<V, E> graphClone = cloneGraph(graph);
+        Graph<V, E> graphCopy = copyGraph(graph);
 
+        // Create a comparator to sort the vertices by ascending degree.
         Comparator<V> byAscendingDegree = (v1, v2) -> {
-            int diff = graphClone.degree(v1) - graphClone.degree(v2);
+            int diff = graphCopy.degree(v1) - graphCopy.degree(v2);
             if (diff == 0) {
                 return v1.compareTo(v2);
             }
             return diff;
         };
 
-        TreeSet<V> vertices = graphClone.getVertices().stream()
+        // Create a sorted set of the vertices.
+        TreeSet<V> vertices = graphCopy.getVertices().stream()
                 .collect(Collectors.toCollection(() -> new TreeSet<>(byAscendingDegree)));
 
         GraphTriangles<E> graphTriangles = new GraphTriangles<>();
 
+        // While there are still vertices to process
+        while (graphCopy.getVertexCount() > 0) {
 
-        while (graphClone.getVertexCount() > 0) {
+            // Get the lowest degree vertex and get its neighbors
             V v = vertices.pollFirst();
-            Collection<V> neighborCollection = graphClone.getNeighbors(v);
+            Collection<V> neighborCollection = graphCopy.getNeighbors(v);
+
             if (neighborCollection != null) {
                 ArrayList<V> neighbors = neighborCollection.stream().collect(Collectors.toCollection(ArrayList::new));
 
+                // For each pair of neighbors, if they are neighbors of each other, increment the triangle count.
                 for (int i = 0; i < neighbors.size() - 1; i++) {
                     for (int j = i + 1; j < neighbors.size(); j++) {
-                        if (graphClone.isNeighbor(neighbors.get(i), neighbors.get(j))) {
-                            graphTriangles.incrementTriangleCount(graphClone.findEdge(v, neighbors.get(i)));
-                            graphTriangles.incrementTriangleCount(graphClone.findEdge(v, neighbors.get(j)));
-                            graphTriangles.incrementTriangleCount(graphClone.findEdge(neighbors.get(i), neighbors.get(j)));
+                        if (graphCopy.isNeighbor(neighbors.get(i), neighbors.get(j))) {
+                            graphTriangles.incrementTriangleCount(graphCopy.findEdge(v, neighbors.get(i)));
+                            graphTriangles.incrementTriangleCount(graphCopy.findEdge(v, neighbors.get(j)));
+                            graphTriangles.incrementTriangleCount(graphCopy.findEdge(neighbors.get(i), neighbors.get(j)));
                         }
                     }
                 }
             }
-            graphClone.removeVertex(v);
+            graphCopy.removeVertex(v);
         }
         return graphTriangles;
     }
 
-    private static <V, E> Graph<V, E> cloneGraph(Graph<V, E> graph) {
+    /**
+     * Creates a new copy of the given graph
+     *
+     * @param graph The graph to copy.
+     * @param <V>   The type of the vertices.
+     * @param <E>   The type of the edges.
+     * @return A copy of the given graph.
+     */
+    private static <V, E> Graph<V, E> copyGraph(Graph<V, E> graph) {
         Graph<V, E> newGraph = new SparseGraph<>();
         graph.getVertices().forEach(newGraph::addVertex);
         graph.getEdges().forEach(e -> {
@@ -115,6 +131,9 @@ public class Triangles {
      * Counts the number of triangles for each edge as well as the total number of edges
      * in a graph using the Fast-Forward counting algorithm.
      *
+     * See T. Schank's dissertation "Algorithmic Aspects of Triangle-Based Network Analysis"
+     * URL: digbib.ubka.uni-karlsruhe.de/volltexte/documents/4541
+     *
      * @param graph The graph on which the triangles are counted.
      * @param <V>   The type of the vertices.
      * @param <E>   The type of the edges.
@@ -123,6 +142,7 @@ public class Triangles {
      * @see GraphTriangles
      */
     public static <V extends Comparable<V>, E> GraphTriangles<E> fastForwardCount(Graph<V, E> graph) {
+        // Create a comparator to sort the vertices by descending degree
         Comparator<V> byDescendingDegree = (v1, v2) -> {
             int diff = graph.degree(v2) - graph.degree(v1);
             if (diff == 0) {
@@ -131,9 +151,12 @@ public class Triangles {
             return diff;
         };
 
+        // Get the set of vertices sorted by descending degree.
         TreeSet<V> vertices = graph.getVertices().stream()
                 .collect(Collectors.toCollection(() -> new TreeSet<>(byDescendingDegree)));
 
+        // Create a map to store a set of vertices associated with each vertex.
+        // This is A(v_i) in the psudocode.
         HashMap<V, Set<V>> vertexMap = new HashMap<>(graph.getVertexCount());
         graph.getVertices().forEach(v -> vertexMap.put(v, new TreeSet<>()));
 
@@ -148,6 +171,8 @@ public class Triangles {
                         Set<V> sSet = vertexMap.get(s);
                         Set<V> tSet = vertexMap.get(t);
                         Set<V> intersection = new TreeSet<>(sSet);
+
+                        // The intersection is the common neighbors
                         intersection.retainAll(tSet);
                         intersection.forEach(v -> {
                             E edgeST = graph.findEdge(s, t);
@@ -163,19 +188,37 @@ public class Triangles {
         return graphTriangles;
     }
 
+    /**
+     * Constants that are used to indicate which counting algorithm to run.
+     */
     public enum CountAlgorithm {
         NODE_ITERATOR, FAST_FORWARD
     }
 
+    /**
+     * GraphTriangles represents the triangles in a graph. It contains methods for storing and retrieving the
+     * number of triangles on each edge as well as the number of triangles in the graph as a whole.
+     * @param <E> The type of the edges.
+     */
     public static class GraphTriangles<E> {
+        // The total number of triangles in the graph.
         private int totalTriangles;
+
+        // Maps the edge to its triangle count.
         private Map<E, Integer> edgeTriangleCountMap;
 
+        /**
+         * Creates an empty GraphTriangles instance.
+         */
         private GraphTriangles() {
             totalTriangles = 0;
             edgeTriangleCountMap = new HashMap<>();
         }
 
+        /**
+         * Increments the number of triangles associated with the given edge.
+         * @param edge The edge whose triangle count needs to be incremented.
+         */
         private void incrementTriangleCount(E edge) {
             Integer numTriangles = edgeTriangleCountMap.get(edge);
             if (numTriangles == null) {
@@ -185,11 +228,19 @@ public class Triangles {
             totalTriangles += 1;
         }
 
+        /**
+         * Returns the total number of triangles in the graph.
+         * @return The total number of triangles in the graph.
+         */
         public int getTotalTriangles() {
             // Each triangle will be counted 3 times.
             return totalTriangles / 3;
         }
 
+        /**
+         * Returns the map containing the number of triangles associated with each edge.
+         * @return The map containing the number of triangles associated with each edge.
+         */
         public Map<E, Integer> getEdgeTriangleCountMap() {
             return edgeTriangleCountMap;
         }
